0f31f90519a1783c2c7c8232cb6e9bbe586f6f56
Mike <michaeljhoelle@gmail.com>
2022-01-18 10:06:50 -0500

    DO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {WordClient} from "./client/WordClient";
 import {WordRowComponent} from "./word-row/word-row.component";
 import {WordRow} from "./model/WordRow";
@@ -18,34 +18,7 @@ export class AppComponent implements OnInit {
   rows: WordRow[] = [];
   currentRow: number = 0;
   currentChar: number = 0;
-  letterStyles: Map<string, string> = new Map<string, string>([
-    ["A", LetterboxStyle.Empty.toString()],
-    ["B", LetterboxStyle.Empty.toString()],
-    ["C", LetterboxStyle.Empty.toString()],
-    ["D", LetterboxStyle.Empty.toString()],
-    ["E", LetterboxStyle.Empty.toString()],
-    ["F", LetterboxStyle.Empty.toString()],
-    ["G", LetterboxStyle.Empty.toString()],
-    ["H", LetterboxStyle.Empty.toString()],
-    ["I", LetterboxStyle.Empty.toString()],
-    ["J", LetterboxStyle.Empty.toString()],
-    ["K", LetterboxStyle.Empty.toString()],
-    ["L", LetterboxStyle.Empty.toString()],
-    ["M", LetterboxStyle.Empty.toString()],
-    ["N", LetterboxStyle.Empty.toString()],
-    ["O", LetterboxStyle.Empty.toString()],
-    ["P", LetterboxStyle.Empty.toString()],
-    ["Q", LetterboxStyle.Empty.toString()],
-    ["R", LetterboxStyle.Empty.toString()],
-    ["S", LetterboxStyle.Empty.toString()],
-    ["T", LetterboxStyle.Empty.toString()],
-    ["U", LetterboxStyle.Empty.toString()],
-    ["V", LetterboxStyle.Empty.toString()],
-    ["W", LetterboxStyle.Empty.toString()],
-    ["X", LetterboxStyle.Empty.toString()],
-    ["Y", LetterboxStyle.Empty.toString()],
-    ["Z", LetterboxStyle.Empty.toString()],
-  ])
+  letterStyles: Map<string, string> = this.getDefaultLetterStyles();
 
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
@@ -76,14 +49,22 @@ export class AppComponent implements OnInit {
               case 0:
               default:
                 style = LetterboxStyle.Wrong;
+                this.letterStyles.set(this.rows[this.currentRow].letters[i].letter, LetterboxStyle.Wrong)
                 winner = false;
                 break;
               case 1:
-                style = LetterboxStyle.Close;
+                if (this.letterIsCorrectElsewhere(data, i)) {
+                  style = LetterboxStyle.Wrong
+                  this.letterStyles.set(this.rows[this.currentRow].letters[i].letter, LetterboxStyle.Wrong)
+                } else {
+                  style = LetterboxStyle.Close;
+                  this.letterStyles.set(this.rows[this.currentRow].letters[i].letter, LetterboxStyle.Close)
+                }
                 winner = false;
                 break;
               case 2:
                 style = LetterboxStyle.Correct;
+                this.letterStyles.set(this.rows[this.currentRow].letters[i].letter, LetterboxStyle.Correct)
             }
             this.rows[this.currentRow].letters[i].style = style;
           }
@@ -101,6 +82,16 @@ export class AppComponent implements OnInit {
           this.displayError("Maybe try typing a real word?");
         }
       });
+  }
+
+  letterIsCorrectElsewhere(result: number[], index: number) {
+    let attemptedLetter = this.rows[this.currentRow].letters[index].letter;
+    for (let i = 0; i < 5 && i != index; i++) {
+      if (this.rows[this.currentRow].letters[i].letter == attemptedLetter && result[i] == 2) {
+        return true;
+      }
+    }
+    return false;
   }
 
   displayError(msg: string) {
@@ -131,6 +122,37 @@ export class AppComponent implements OnInit {
     return /^[a-zA-Z]$/.test(str);
   }
 
+  getDefaultLetterStyles() {
+    return new Map<string, string>([
+      ["A", LetterboxStyle.Empty.toString()],
+      ["B", LetterboxStyle.Empty.toString()],
+      ["C", LetterboxStyle.Empty.toString()],
+      ["D", LetterboxStyle.Empty.toString()],
+      ["E", LetterboxStyle.Empty.toString()],
+      ["F", LetterboxStyle.Empty.toString()],
+      ["G", LetterboxStyle.Empty.toString()],
+      ["H", LetterboxStyle.Empty.toString()],
+      ["I", LetterboxStyle.Empty.toString()],
+      ["J", LetterboxStyle.Empty.toString()],
+      ["K", LetterboxStyle.Empty.toString()],
+      ["L", LetterboxStyle.Empty.toString()],
+      ["M", LetterboxStyle.Empty.toString()],
+      ["N", LetterboxStyle.Empty.toString()],
+      ["O", LetterboxStyle.Empty.toString()],
+      ["P", LetterboxStyle.Empty.toString()],
+      ["Q", LetterboxStyle.Empty.toString()],
+      ["R", LetterboxStyle.Empty.toString()],
+      ["S", LetterboxStyle.Empty.toString()],
+      ["T", LetterboxStyle.Empty.toString()],
+      ["U", LetterboxStyle.Empty.toString()],
+      ["V", LetterboxStyle.Empty.toString()],
+      ["W", LetterboxStyle.Empty.toString()],
+      ["X", LetterboxStyle.Empty.toString()],
+      ["Y", LetterboxStyle.Empty.toString()],
+      ["Z", LetterboxStyle.Empty.toString()],
+    ])
+  }
+
   reset() {
     this.rows = [];
     let newRow = new WordRowComponent();
@@ -138,5 +160,6 @@ export class AppComponent implements OnInit {
     this.rows.push(newRow);
     this.currentChar = 0;
     this.currentRow = 0;
+    this.letterStyles = this.getDefaultLetterStyles();
   }
 }
