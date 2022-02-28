07690640df23914af1a47148cf8bfcb2c355af39
Mike <michaeljhoelle@gmail.com>
2022-02-27 20:18:50 -0500

    ??????

--- src/app/winner-box/winner-box.component.ts
+++ src/app/winner-box/winner-box.component.ts
@@ -62,10 +62,7 @@ export class WinnerBoxComponent implements OnInit {
   getResult() {
     let tries = this.result.length
     let triesText = this.result.length > 1 ? " tries" : " try";
-    let out = "Wordle 2\r\n" + tries + triesText + "\r\n";
-    if (tries == 69) {
-      out = out.concat("nice\r\n")
-    }
+    let out = "Wordle 2\r\n" + tries + triesText + "\r\n" + this.rank + "\r\n";
     for (let i = 0; i < this.result.length; i++) {
       let str = this.result[i].letters.map(s => {
         switch (s.style) {
